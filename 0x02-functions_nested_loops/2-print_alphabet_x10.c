ba7dff78379d2695e0f56e17192e638f383acc5c
Mmog12 <kamalmogdad@gmail.com>
2022-12-14 11:19:01 -0800

    hello

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char m;
+    int n;
+	
+    for (m = 'a'; m <= 'z'; m++)
+    {	    
+	    _putchar(m, '\n');
+	    for (n = 0; n < 10; n++);
+    }
+    _putchat('\n');
+    return (0);
+}
