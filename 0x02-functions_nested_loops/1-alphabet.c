4b98d010c8518bbfc8830f203e401c81cbcda1be
Mmog12 <kamalmogdad@gmail.com>
2022-12-14 11:00:42 -0800

    hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,7 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * print_alphapet - prints the alpha in lowercase
+ * Return: Always 0
  */
 
 void print_alphapet(void)
@@ -8,8 +9,9 @@ void print_alphapet(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
-	
+	}
 	_putchar('\n');
 
 }
