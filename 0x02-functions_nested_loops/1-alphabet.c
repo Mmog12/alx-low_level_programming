fba91f38d292821b0b293aa4d90be26edf3c8bb3
Mmog12 <kamalmogdad@gmail.com>
2022-12-14 09:53:48 -0800

    hello world

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphapet - entry point
+ * Return: 0
+ */
+void print_alphapet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n')
+
+}
