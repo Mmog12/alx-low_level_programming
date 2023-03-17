81392c1c505750977bff43636710d535e154d8f4
Almegdad kamal <89933289+Mmog12@users.noreply.github.com>
2023-03-17 20:05:54 +0200

    Update 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,20 +1,4 @@
-husambe
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x17-doubly_linked_lists/4-free_dlistint.c
-@husambe
-husambe Create 4-free_dlistint.c
- 1 contributor
-22 lines (19 sloc)  312 Bytes
-/*
+ /*
  * File: 4-free_dlistint.c
  * Auth: Brennan D Baraban
  */
